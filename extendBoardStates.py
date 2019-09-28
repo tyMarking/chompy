7d298cd88c8fe1c0461955f863da2fbfb49f3612
SharpNeutrons <hunterparsons3@gmail.com>
2019-09-28 01:25:06 -0400

    Mid testing of extendBoardStates. Add Row is maybe done.

--- extendBoardStates.py
+++ extendBoardStates.py
@@ -1,23 +1,37 @@
 import utility as util
+import numpy as np
 
 def appendRowToBoardStates(oldStates):
 	newStates = []
 	for oldState in oldStates:
 		file = util.file(oldState)
 		n = len(oldState)
-		m = len(oldState[0] + 1)
+		# m = len(oldState[0] + 1)
+		maxCol = 0
+
+		blankNewState = np.insert(oldState, 0, 0, axis=0)
+		print(blankNewState)
 		#if the oldState does not have any bites or bites only
 		#from the first column, the bites in the new row can be from
 		#any column up to n
 		if file == 0 or file == 1:
-			newState = addBlankRowToBoardState(oldState, m)
 			if file == 0:
-				newStates.append(newState)
-			for i in range(n):
-				print(i)				
+				#for the special case of 0, add the blank state
+				newStates.append(blankNewState)
+			maxCol = n
+		#if the oldState has bites taken out of it that aren't from the end
+		#or beginning, the bites in the new row can be from any column
+		#from m to the file
+		elif not file == n:
+			maxCol = file
+		#for file == n, the default maxCol 0, requires that only the bite with
+		#the whole row is taken.	
+		for i in range(maxCol):
+			addNewBittenRow(newStates, blankNewState, i)
+	return newStates
 
-def addBlankRowToBoardState(oldState, m):
-	newRow = []
-	for i in range(m):
-		newRow[i] = 0
-	return util.copy(oldState).insert(0, newRow)
+def addNewBittenRow(newStates, blankNewState, col):
+	newState = util.copy(blankNewState)
+	util.bite(newState, [0, col])
+	print(newState)
+	newStates.append(newState)
--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 import utility as util
+import numpy as np
+import extendBoardStates as ebs
 """
 board = util.genBoard(3,5)
 board[0][4] = 1
@@ -21,16 +23,33 @@ print(data[0][2])
 
 """
 
-b1, b2 = util.genBoard(3,5), util.genBoard(3,5)
-util.bite(b1, (0,4))
-util.bite(b1, (2,4))
-util.bite(b2, (1,3))
+# b1, b2 = util.genBoard(3,5), util.genBoard(3,5)
+# util.bite(b1, (0,4))
+# util.bite(b1, (2,4))
+# util.bite(b2, (1,3))
 
-print("B1")
-util.display(b1)
-print("B2")
-util.display(b2)
+# print("B1")
+# util.display(b1)
+# print("B2")
+# util.display(b2)
 
-print(util.delta(b1,b2))
-print(util.isRelated(b1,b2))
-print(util.isDecendent(b2,b1))
+# print(util.delta(b1,b2))
+# print(util.isRelated(b1,b2))
+# print(util.isDecendent(b2,b1))
+
+#arr = [[1, 1, 1], [2, 2, 2], [3, 3, 3], [4, 4, 4]]
+#added = np.insert(arr, 0, 0, axis=0)
+#print(added)
+
+arr = [
+	[[0 ,1],
+	 [-1,0]],
+	[[1 ,1],
+	 [-1,0]],
+	[[0 ,1],
+	 [-1,1]],
+	[[1 ,1],
+	 [-1,1]]
+]
+newStates = ebs.appendRowToBoardStates(arr)
+# print(newStates)
