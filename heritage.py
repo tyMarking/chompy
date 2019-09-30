01cfe4474d1c02124f8b0d70231505beb693e5d7
Ty Marking <30440916+tyMarking@users.noreply.github.com>
2019-09-29 20:49:44 -0400

    Got heritage function working

--- heritage.py
+++ heritage.py
@@ -10,49 +10,33 @@ def getHeritage(states):
 
 		bites = util.gamma(state)
 		for bite in bites:
-			#If cell to left and to bottow are both bitten then skip
-			if bite[1] > 0 and state[bite[0]][bite[1]-1] == 1 and bite[0] < len(state)-1 and state[bite[0]+1][bite[1]]:
-				continue
+
+			if not (bite[0] == len(state)-1 or bite[1] == 0 or (state[bite[0]+1][bite[1]-1] == 0 and not (state[bite[0]][bite[1]-1] == 1 and state[bite[0]+1][bite[1]] == 1)) ):
+				#Check for square shit
+
+
+				
+				#don't ask
+				#if bite[1] > 0 and state[bite[0]][bite[1]-1] == 1 and bite[0] < len(state)-1 and state[bite[0]+1][bite[1]] and state[bite[0]+1][bite[1]-1] == 0:
+					#continue
+				#print("bite: " + str(bite))
+				newRow = bite[0] + 1
+				newCol = bite[1] - 1
+				while newRow < len(state) and newCol > -1 and state[newRow][newCol] == 1:
+					#print("itered")
+					newRow += 1
+					newCol -= 1
+				if newRow != len(state) and newCol != 0:
+					#print("New row and col: " + str(newRow) + ", " + str(newCol))
+					#if not on edge and cell to left or down of bite outside square == 1
+					if (state[bite[0]][newCol] == 1 or state[newRow][bite[1]] == 1):
+						#print("Continued")
+						continue
+					#if bite[1] > 0 and state[bite[0]][bite[1]-1] == 1 and bite[0] < len(state)-1 and state[bite[0]+1][bite[1]] and state[bite[0]+1][bite[1]-1] == 0:
+
+				
+
 			parent = np.copy(state)
 			util.unBite(parent, bite)
 			stateXparents[util.dKey(state)].append(parent)
 	return stateXparents
-
-
-
-
-	"""
-	for state in states:
-		stateXparents[util.dKey(state)] = [] 
-		bites = util.gamma(state)
-		print("Bites: ")
-		print(bites)
-		print("\n\n")
-		for bite in bites:
-			newBites = [a[:] for a in bites]
-			newBites.remove(bite)
-
-			print("new bites: ")
-			print(newBites)
-			parent = util.superimpose( (len(state), len(state[0])), newBites)
-			print("parent: ")
-			util.display(parent)
-			if np.all(parent == state):
-				continue
-			
-			print("parents from dict")
-			print(stateXparents[util.dKey(state)])
-			print("parent")
-			print(parent)
-			
-			isIn = False
-
-			for dictParent in stateXparents[util.dKey(state)]:
-				if np.all(parent == dictParent):
-					isIn = True
-					break
-
-			if not isIn:
-				stateXparents[util.dKey(state)].append(parent)
-		return stateXparents
-	"""
